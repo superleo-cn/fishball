4ac789ab057af9513050611b231e0c5b7a16bc45
LEO HU <superleo.peking@gmail.com>
2013-10-26 20:54:10 +0800

    commit

--- app/controllers/Basic.java
+++ app/controllers/Basic.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controllers;
 
 import inteceptors.ConstantsInterceptor;
@@ -24,7 +23,7 @@ import constants.Constants;
 @With(value = { TimeInterceptor.class, ConstantsInterceptor.class })
 public class Basic extends Controller {
 
-	@Before(unless = { "Auth.index", "Auth.login", "Auth.logout"})
+	@Before(unless = { "Auth.index", "Auth.login", "Auth.logout" })
 	static void checkAuthentification() {
 		if (StringUtils.isEmpty(session.get(Constants.CURRENT_USERNAME))) {
 			// flash.put("error", "You need login first");
@@ -61,146 +60,42 @@ public class Basic extends Controller {
 		renderJSON(map);
 	}
 
-
-    @Before(only = {"Employees.index","Customers.index","Suppliers.index"})
-    public static void navigationAdd(){
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("add",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-    @Before(only = {"Employees.view","Customers.view","Suppliers.view"})
-    public static void navigationSave(){
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("save",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-    @Before(only = {"Employees.index","Customers.index","Suppliers.index"})
-    public static void navigationList(){
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("list",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-    @Before(only = {"Home.index","Auth.index","Auth.login"})
-    public static void navigationNoBack(){
-
-        System.out.println("x");
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("noback",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-=======
-package controllers;
-
-import inteceptors.ConstantsInterceptor;
-import inteceptors.TimeInterceptor;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
-
-import play.Logger;
-import play.Play;
-import play.mvc.Before;
-import play.mvc.Catch;
-import play.mvc.Controller;
-import play.mvc.Finally;
-import play.mvc.With;
-import constants.Constants;
-
-@With(value = { TimeInterceptor.class, ConstantsInterceptor.class })
-public class Basic extends Controller {
-
-	@Before(unless = { "Auth.index", "Auth.login", "Auth.logout"})
-	static void checkAuthentification() {
-		if (StringUtils.isEmpty(session.get(Constants.CURRENT_USERNAME))) {
-			// flash.put("error", "You need login first");
-			Auth.index();
-		}
-		// TODO: refactor
-		// Cache.safeSet(Constants.GLOBLE_FACILITIES, Facility.findAll(),
-		// "365d");
+	@Before(only = { "Employees.index", "Customers.index", "Suppliers.index" })
+	public static void navigationAdd() {
+		Map navigation = (Map) renderArgs.get("navigation");
+		if (navigation == null)
+			navigation = new HashMap();
+		navigation.put("add", true);
+		renderArgs.put("navigation", navigation);
 	}
 
-	@Catch(Exception.class)
-	public static void exception(Throwable e) {
-		Logger.error("error happend.", e);
+	@Before(only = { "Employees.view", "Customers.view", "Suppliers.view" })
+	public static void navigationSave() {
+		Map navigation = (Map) renderArgs.get("navigation");
+		if (navigation == null)
+			navigation = new HashMap();
+		navigation.put("save", true);
+		renderArgs.put("navigation", navigation);
 	}
 
-	@Finally
-	public static void finalExecute() {
-
+	@Before(only = { "Employees.index", "Customers.index", "Suppliers.index" })
+	public static void navigationList() {
+		Map navigation = (Map) renderArgs.get("navigation");
+		if (navigation == null)
+			navigation = new HashMap();
+		navigation.put("list", true);
+		renderArgs.put("navigation", navigation);
 	}
 
-	public static void uploadFile(File imgFile) {
-		File file = Play.getFile("/public/uploads");
-		String filename = imgFile.getName();
-		Map map = new HashMap();
-		try {
-			FileUtils.copyFileToDirectory(imgFile, file);
-			map.put("error", 0);
-			map.put("url", Play.ctxPath + "/public/uploads/" + filename);
-		} catch (IOException e) {
-			Logger.error("upload file error", e);
-			map.put("error", 1);
-			map.put("url", Play.ctxPath + "/public/uploads/error.jpg");
-		}
-		renderJSON(map);
+	@Before(only = { "Home.index", "Auth.index", "Auth.login" })
+	public static void navigationNoBack() {
+
+		System.out.println("x");
+		Map navigation = (Map) renderArgs.get("navigation");
+		if (navigation == null)
+			navigation = new HashMap();
+		navigation.put("noback", true);
+		renderArgs.put("navigation", navigation);
 	}
 
-
-    @Before(only = {"Employees.index"})
-    public static void navigationAdd(){
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("add",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-    @Before(only = {"Employees.add","Employees.view"})
-    public static void navigationSave(){
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("save",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-    @Before(only = {"Employees.index"})
-    public static void navigationList(){
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("list",true);
-        renderArgs.put("navigation", navigation);
-    }
-
-    @Before(only = {"Home.index","Auth.index","Auth.login"})
-    public static void navigationNoBack(){
-
-        System.out.println("x");
-        Map navigation = (Map) renderArgs.get("navigation");
-        if(navigation==null)
-            navigation = new HashMap();
-        navigation.put("noback",true);
-        renderArgs.put("navigation", navigation);
-    }
-
->>>>>>> 94621b5f64090df2b83485801b6d7879459206b4
 }
